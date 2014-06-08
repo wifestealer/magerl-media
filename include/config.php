c8346aa6b4a484541eb89d99c36f4df152cb55a6
Karl Magerl <magerl@webstollen.de>
2014-06-08 16:48:18 +0200

    Konfiguration auf lokale Entwicklungsumgebung angepasst

--- include/config.php
+++ include/config.php
@@ -6,6 +6,6 @@
  * Time: 19:58
  */
 
-const SEITE_URL = "http://localhost/magerl-media";
+const SEITE_URL = "http://localhost/projects/magerl-media-site/";
 const SEITE_MAIL = "mail@magerl-media.de";
-const SEITE_PFAD = "C:/Program Files (x86)/VertrigoServ/www/magerl-media/";
+const SEITE_PFAD = "C:\Program Files (x86)\EasyPHP-DevServer-14.1VC9\data\localweb\projects\magerl-media-site";
